1ed2c1f2f53e499f4938364350cf5702b0490e9d
SomaTherion <nocturne_wof@hotmail.es>
2019-12-13 18:56:36 +0100

    final??

--- __manifest__.py
+++ __manifest__.py
@@ -5,5 +5,5 @@
     'version': '1.0',
     'website': 'iesayala.ddns.net',
     'depends': ['base'],
-    'data': ['views/empresa.xml','views/departamento.xml','views/empleado.xml','views/report.xml']
+    'data': ['views/empresa.xml','views/departamento.xml','views/empleado.xml','views/report.xml','views/instituto.xml','views/asignatura.xml','views/alumno.xml' ]
 }
